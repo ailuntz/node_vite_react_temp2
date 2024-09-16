a041961d0748a23fac0f526512ea4f9cecd1ac14
ailuntz <ailuntz@icloud.com>
2024-09-16 13:37:18 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { appWindow } from '@tauri-apps/api/window'
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
