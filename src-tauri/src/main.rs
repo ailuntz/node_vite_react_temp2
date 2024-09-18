7bb16dc16654d173ec6286e027b0944cf87f6bf5
ailuntz <ailuntz@icloud.com>
2024-09-18 05:07:01 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,21 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// fn main() {
-//   tauri::Builder::default()
-//     .run(tauri::generate_context!())
-//     .expect("error while running tauri application");
-// }
-
-mod setup;
-
-// fn main() {
-//     let context = tauri::generate_context!();
-//     tauri::Builder::default()
-//         .setup(setup::init)
-//         .run(context)
-//         .expect("error while running OhMyBox application");
-// }
+use tauri::Manager;
+use window_vibrancy::*;
 
 fn main() {
     tauri::Builder::default()
-        .setup(setup::init)
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            #[cfg(target_os = "windows")]
+            apply_blur(&window, Some((18, 18, 18, 125)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
