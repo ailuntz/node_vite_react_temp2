3343167c4ca45041377ce5697b5a8a4a5f7c499e
ailuntz <ailuntz@icloud.com>
2024-09-18 04:31:36 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,6 +1,5 @@
 use tauri::{App, Manager};
 use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
-use raw_window_handle::HasRawWindowHandle;
 
 /// setup
 pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -9,16 +8,19 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
     // 仅在 macOS 下执行
     #[cfg(target_os = "macos")]
     {
-        // 使用 raw_window_handle() 获取原始窗口句柄
-        let raw_handle = win.raw_window_handle();
+        // 使用 ns_view 获取原始窗口句柄
+        let raw_handle = win.ns_view() as *mut std::ffi::c_void;
         apply_vibrancy(raw_handle, NSVisualEffectMaterial::FullScreenUI, None, None)
             .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
     }
 
     // 仅在 Windows 下执行
     #[cfg(target_os = "windows")]
-    apply_blur(&win, Some((18, 18, 18, 125)))
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+    {
+        let raw_handle = win.hwnd() as *mut std::ffi::c_void;
+        apply_blur(&raw_handle, Some((18, 18, 18, 125)))
+            .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+    }
 
     Ok(())
 }
