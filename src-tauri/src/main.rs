bedc565562a85ea657cac74a50f9827963dd06d7
ailuntz <ailuntz@icloud.com>
2024-09-18 11:11:57 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@
             let window = app.get_window("main").unwrap();
   
             #[cfg(target_os = "macos")]
-            apply_vibrancy(&window, NSVisualEffectMaterial::Light, Some(NSVisualEffectState::Active), Some(0.06))
+            apply_vibrancy(&window, NSVisualEffectMaterial::Titlebar, Some(NSVisualEffectState::Active), Some(0.06))
                 .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
   
             #[cfg(target_os = "windows")]
