91e23e74848c84a8d5df07ebb09c51f7a9eb9a6a
ailuntz <ailuntz@icloud.com>
2024-09-18 03:25:44 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// fn main() {
+//   tauri::Builder::default()
+//     .run(tauri::generate_context!())
+//     .expect("error while running tauri application");
+// }
+
+use tauri::Manager;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            tauri_plugin_vibrancy::apply_vibrancy(&window, tauri_plugin_vibrancy::Vibrancy::Acrylic, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only available on macOS and Windows");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
