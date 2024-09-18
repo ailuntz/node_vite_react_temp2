36c4409ccc680ef33adfe8d6a78e6cd4fb2da9ca
ailuntz <ailuntz@icloud.com>
2024-09-18 03:34:04 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,16 +7,13 @@
 //     .expect("error while running tauri application");
 // }
 
-use tauri::Manager;
+mod setup;
 
 fn main() {
+    let context = tauri::generate_context!();
     tauri::Builder::default()
-        .setup(|app| {
-            let window = app.get_window("main").unwrap();
-            tauri_plugin_vibrancy::apply_vibrancy(&window, tauri_plugin_vibrancy::Vibrancy::Acrylic, None)
-                .expect("Unsupported platform! 'apply_vibrancy' is only available on macOS and Windows");
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(setup::init)
+        .run(context)
+        .expect("error while running OhMyBox application");
 }
+
