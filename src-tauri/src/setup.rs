5c4f0ab509e71bf33ad66088839b390521bfa7ea
ailuntz <ailuntz@icloud.com>
2024-09-18 04:26:18 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,5 +1,6 @@
 use tauri::{App, Manager};
 use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
+use raw_window_handle::HasRawWindowHandle;
 
 /// setup
 pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -7,9 +8,9 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
 
     // 仅在 macOS 下执行
     #[cfg(target_os = "macos")]
-    unsafe {
-        // 使用 ns_window() 获取 macOS 上的原始 NSWindow 对象
-        let raw_handle = win.ns_window().unwrap() as *mut std::ffi::c_void;
+    {
+        // 使用 raw_window_handle() 获取原始窗口句柄
+        let raw_handle = win.raw_window_handle();
         apply_vibrancy(raw_handle, NSVisualEffectMaterial::FullScreenUI, None, None)
             .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
     }
