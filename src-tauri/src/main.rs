e2cb00ba8882a33641478994444c3b97790f0657
ailuntz <ailuntz@icloud.com>
2024-09-18 08:21:28 +0000

    Update GitHub Actions workflow with Tauri configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,11 +20,11 @@ fn main() {
 
 
           #[cfg(target_os = "macos")]
-          apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+          apply_vibrancy(&raw_handle, NSVisualEffectMaterial::HudWindow, None, None)
               .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
           #[cfg(target_os = "windows")]
-          apply_blur(&window, Some((18, 18, 18, 125)))
+          apply_blur(&raw_handle, Some((18, 18, 18, 125)))
               .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
           Ok(())
